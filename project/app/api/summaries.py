d15835b50c8e155938c8dd526af2a01210db01d9
JLCodeSource <JLCodeSource@gmail.com>
2022-05-18 13:26:21 +0100

    Part 1 Completed

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -2,13 +2,15 @@ from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
-
+from app.models.tortoise import SummarySchema
 
 router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+async def create_summary(
+    payload: SummaryPayloadSchema
+) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
 
     response_object = {
@@ -16,3 +18,17 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
         "url": payload.url
     }
     return response_object
+
+
+@router.get("/", response_model=list[SummarySchema])
+async def read_all_summaries() -> list[SummarySchema]:
+    return await crud.get_all()
+
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    return summary
