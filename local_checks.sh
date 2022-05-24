7acffc75aca783aecae07e46b87645441a87d813
JLCodeSource <JLCodeSource@gmail.com>
2022-05-24 11:29:01 +0100

    Commit additional checks

--- local_checks.sh
+++ local_checks.sh
@@ -10,3 +10,13 @@ echo "--- docker-compose exec web python -m isort . --check-only"
 docker-compose exec web python -m isort . --check-only
 echo "--- docker-compose exec web bandit -r ."
 docker-compose exec web bandit -r .
+echo "--- docker-compose exec safety check"
+docker-compose exec safety check
+echo "--- docker-compose exec safety check -r requirements.txt"
+docker-compose exec safety check -r requirements.txt
+echo "--- docker-compose exec safety check -r requirements-dev.txt"
+docker-compose exec safety check -r requirements-dev.txt
+echo "--- trivy security check web"
+trivy image fastapi-testdriven_web
+echo "--- trivy security check web_db"
+trivy image fastapi-testdriven_web-db
