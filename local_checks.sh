a6024fb11096095c5ce21e25da746d4b6866eef8
JLCodeSource <JLCodeSource@gmail.com>
2022-05-24 09:19:12 +0100

    Added bandit code security review

--- local_checks.sh
+++ local_checks.sh
@@ -8,3 +8,5 @@ echo "--- docker-compose exec web python -m black . --check"
 docker-compose exec web python -m black . --check
 echo "--- docker-compose exec web python -m isort . --check-only"
 docker-compose exec web python -m isort . --check-only
+echo "--- docker-compose exec web bandit -r ."
+docker-compose exec web bandit -r .
